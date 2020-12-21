8f131b8d2a51e95f74eb64874ea1d21fc8b43015
CGC <70499508+cgconverse@users.noreply.github.com>
2020-12-21 13:00:33 -0800

    created variables for API key and query URL and created  AJAX call to OpenWeatherMap API

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Weather-Dashboard
+
+Button to trigger API call for user input
+
+Append to cities div w/ JQuery 
+
+for (i=0; response.length; i += 8 >){
+    
+}
---
+++ script.js
@@ -0,0 +1,24 @@
+// console.log('hello');
+
+var apiKey = '44caa7ffd0618edcab3a952d49f6d96c'
+
+//This builds the URL needed to query the database
+var queryURL = `api.openweathermap.org/data/2.5/forecast?q=Sacramento&appid=${apiKey}`
+
+//This runs the AJAX call to the OpenWeatherMap API
+$.ajax({
+    url: queryURL,
+    method: "GET"
+})
+
+//This stores all the retrieved data inside of an object named "response"
+.then(function(response) {
+
+    //Log the queryURL
+    console.log(queryURL);
+
+    //Log the resulting object
+    console.log(response);
+
+    //Transfer content to HTML
+}
